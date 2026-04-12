c6b6ec21cc7d7277fa43b51f8a8416a17dd372ec
Santi G <santigracia@santigracia.com>
2026-04-12 07:46:36 +0200

    Initial Commit

---
+++ issue522/issue522App.swift
@@ -0,0 +1,17 @@
+//
+//  issue522App.swift
+//  issue522
+//
+//  Created by Santi Gracia on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct issue522App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
